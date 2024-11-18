35f8b75bfa861d4250c75c440a9067e352631251
AlessandrodeAlmeida2 <agostini55@gmail.com>
2024-11-18 15:54:33 -0300

    Add pop up container 2

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ $(document).ready(function () {
         $("nav ul").slideToggle(); // Update the selector here
     });
 });
+
+$(document).ready(function () {
+    // Cria a estrutura do pop-up
+    const popupOverlay = $('<div class="popup-overlay"></div>');
+    const popup = $(`
+      <div class="popup">
+        <p id="popup-text"></p>
+        <span class="popup-close">Fechar</span>
+      </div>
+    `);
+    $("body").append(popupOverlay).append(popup);
+  
+    // Mostra o pop-up com o texto correspondente
+    $(".popup-trigger").on("click", function () {
+      const description = $(this).data("description");
+      $("#popup-text").text(description);
+      popupOverlay.fadeIn();
+      popup.fadeIn();
+    });
+  
+    // Fecha o pop-up
+    $(".popup-close, .popup-overlay").on("click", function () {
+      popupOverlay.fadeOut();
+      popup.fadeOut();
+    });
+  });
+  
